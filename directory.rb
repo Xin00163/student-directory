1fb41a25790b26cdfaf7a2ea7409dd38fd46c149
Xin Wang <wangxin00163@yahoo.co.uk>
2017-11-07 22:25:20 +0000

    exercise 5

--- directory.rb
+++ directory.rb
@@ -1,16 +1,24 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-
   students = []
   name = gets.chomp
+  puts "What is " + name + "'s hobby?"
+  hobbies = gets.chomp
+  puts "Which country was " + name + " born?"
+  country_of_birth = gets.chomp
+  puts "What is " + name + "'s height?"
+  height = gets.chomp
+
   #while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: :november, hobbies: hobbies,
+      country_of_birth: country_of_birth, height: height}
     puts "Now we have #{students.count} students"
     #get another name from the user
     name = gets.chomp
+
   end
   # return the array of students
   students
@@ -21,7 +29,7 @@ def print_header
   puts "---------------"
 end
 
-# ex2. print the students whose name begins with a specific letter
+#ex2. print the students whose name begins with a specific letter
 def print_first_letter(students)
   puts "What is the first letter?"
   first_letter = gets.chomp.downcase
@@ -33,11 +41,26 @@ def print_first_letter(students)
   end
 end
 
+# ex4. while method
+def print_while(students)
+  i = 0
+  while i < students.length
+    puts "#{students[i][:name]} (#{students[i][:cohort]} cohort)"
+    puts "Hobby: #{students[i][:hobbies]}"
+    puts "Birth country : #{students[i][:country_of_birth]}"
+    puts "Height is #{students[i][:height]}"
+    i += 1
+  end
+end
+
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
-students = input_students #store the results of the method in the variable called students?
+#notes: students in the method input_students could only be used inside the method.
+#Here is to create a students varibla to store the result of the method, so that it could be passed to others.
+students = input_students
 print_header
 print_first_letter(students)
+print_while(students)
 print_footer(students)
