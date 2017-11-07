6f2a2f11344e873d336658a0f7392de461abf7f3
Xin Wang <wangxin00163@yahoo.co.uk>
2017-11-07 09:19:53 +0000

    Use array to avoid repetition

--- directory.rb
+++ directory.rb
@@ -1,18 +1,20 @@
-studnet_count = 11
 #first we print the list of students
+students = [
+"Dr. Hannibal Lecter",
+"Darth Vader",
+"Nurse Ratched",
+"Michael Corleone",
+"Ales DeLarge",
+"The Wicked Witch of the West",
+"Terminator",
+"Freddy Krueger",
+"The Joker",
+"Joffrey Baratheon",
+"Norman Bates"]
+
 puts "The students of D Academy"
-puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Ales DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-# finally, we print the total number of students
-#it's important thant print() doesn't add new line characters.
-puts "Overall, we have #{studnet_count} great students"
+puts "---------------"
+students.each do |student|
+  puts student
+end
+puts "Overall, we have #{students.count} great students"
