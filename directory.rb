3fc845aeab456786b0f5957042d450d8e472720a
Xin Wang <wangxin00163@yahoo.co.uk>
2017-11-13 11:45:21 +0000

    Step 14.2

--- directory.rb
+++ directory.rb
@@ -84,7 +84,7 @@ end
 
 def try_load_students
   filename = ARGV.first# first argument from the command line
-  return if filename.nil?# get out of the method if it isn't given
+  return load_students if filename.nil?# load_students if no file is given
   if File.exists?(filename)
     load_students(filename)
     puts "Loaded #{@students.count} from #{filename}"
